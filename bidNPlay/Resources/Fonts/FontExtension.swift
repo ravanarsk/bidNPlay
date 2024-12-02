5b89de6bae6b1aaadd52a191b0a58defd5a48687
Ashin Asok <ashin@sectorqube.com>
2024-12-02 13:50:00 +0530

    color and fonts

---
+++ bidNPlay/Resources/Fonts/FontExtension.swift
@@ -0,0 +1,32 @@
+//
+//  FontExtension.swift
+//  bidNPlay
+//
+//  Created by SectorQube Tech Mini on 02/12/24.
+//
+
+import UIKit
+
+extension UIFont{
+    
+    internal func boldFontWith(size: CGFloat) -> UIFont{
+        return UIFont(name: "Poppins-Bold", size: size) ?? UIFont.systemFont(ofSize: size,weight: .bold)
+    }
+    
+    internal func regularFontWith(size: CGFloat) -> UIFont{
+        return UIFont(name: "Poppins-Regular", size: size) ?? UIFont.systemFont(ofSize: size,weight: .regular)
+    }
+    
+    internal func lightFontWith(size: CGFloat) -> UIFont{
+        return UIFont(name: "Poppins-Light", size: size) ?? UIFont.systemFont(ofSize: size,weight: .light)
+    }
+    
+    internal func semiBoldFontWith(size: CGFloat) -> UIFont{
+        return UIFont(name: "Poppins-SemiBold", size: size) ?? UIFont.systemFont(ofSize: size,weight: .semibold)
+    }
+    
+    internal func mediumFontWith(size: CGFloat) -> UIFont{
+        return UIFont(name: "Poppins-Medium", size: size) ?? UIFont.systemFont(ofSize: size,weight: .medium)
+    }
+    
+}
---
+++ bidNPlay/Utils/Colors/CustomColor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColor.swift
+//  Ikin Business
+//
+//  Created by SQT MAC PRO on 28/04/23.
+//
+
+import UIKit
+
+enum CustomColor{
+    
+    static let bg = UIColor(named: "bgColor") ?? UIColor.black
+    static let bg2 = UIColor(named: "bgColor2") ?? UIColor.black
+    static let button = UIColor(named: "buttonColor") ?? UIColor.blue
+    static let text = UIColor(named: "textColor") ?? UIColor.white
+    static let text2 = UIColor(named: "textColor") ?? UIColor.lightGray
+    
+}
